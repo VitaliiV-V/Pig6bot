0e3b3be72728130687d152de991b0d218365a71b
VitaliiV-V <mforcodeforces@rambler.ru>
2026-06-30 20:29:41 +0300

    initial commit

--- settings.py
+++ settings.py
@@ -5,4 +5,11 @@ load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 JUDGMENT_DAY_TOKEN = os.getenv("JUDGMENT_DAY_TOKEN")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+OWNER_ID = int(os.getenv("OWNER_ID"))
+MAIN_CHANNEL_ID = int(os.getenv("MAIN_CHANNEL_ID"))
+PERSONAL_CHANNEL_ID = int(os.getenv("PERSONAL_CHANNEL_ID"))
+LOGS_CHANNEL_ID = int(os.getenv("LOGS_CHANNEL_ID"))
+FULL_CHANNEL_ID = int(os.getenv("FULL_CHANNEL_ID"))
+ENABLE_TEXT_NOTIFICATIONS = os.getenv("ENABLE_TEXT_NOTIFICATIONS") == True
+OWNER_NAME = os.getenv("OWNER_NAME")
